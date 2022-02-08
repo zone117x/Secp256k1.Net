400ea62fba9e4e0b11adf4fc0a7b1ae283f1f3c2
Matthew Little <zone117x@gmail.com>
2022-02-08 01:11:03 +0100

    feat: linux-arm64 and macos-arm64 (Apple Silicon) support (#14)

--- Secp256k1.Net/LibPathResolver.cs
+++ Secp256k1.Net/LibPathResolver.cs
@@ -20,7 +20,9 @@ namespace Secp256k1Net
             [(Windows, X64)] = ("win-x64", "", ".dll"),
             [(Windows, X86)] = ("win-x86", "", ".dll"),
             [(Linux, X64)] = ("linux-x64", "lib", ".so"),
+            [(Linux, Arm64)] = ("linux-arm64", "lib", ".so"),
             [(OSX, X64)] = ("osx-x64", "lib", ".dylib"),
+            [(OSX, Arm64)] = ("osx-arm64", "lib", ".dylib"),
         };
 
         static readonly OSPlatform[] SupportedPlatforms = { Windows, OSX, Linux };

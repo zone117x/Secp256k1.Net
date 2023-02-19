48639f840abef56f0d415cc9c4b11d8c73110279
Matthew Little <zone117x@gmail.com>
2023-02-19 15:23:21 +0100

    chore: cleanup benchmarks

--- Secp256k1.Net.Bench/Program.cs
+++ Secp256k1.Net.Bench/Program.cs
@@ -1,190 +1,258 @@
 ﻿using System;
 using System.Text;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Jobs;
-using SHA256 = System.Security.Cryptography.SHA256;
-using System.Linq;
-using NethereumSigner = Nethereum.Signer;
+using BenchmarkDotNet.Configs;
 
 namespace Secp256k1Net.Bench
 {
-    [CsvMeasurementsExporter]
-    public class EcdsaSignVerify
+    record class KeyPair(byte[] PrivateKey, byte[] PublicKeyCompressed, byte[] PublicKeyUncompressed);
+    record class Msg(string MsgString, byte[] MsgBytes, byte[] MsgHash);
+
+    class BenchInputs
     {
-        private readonly string msg;
-        private readonly byte[] msgBytes;
-        private readonly (byte[] PrivateKey, byte[] PublicKeyCompressed, byte[] PublicKeyUncompressed) keyPair;
-        
-        public EcdsaSignVerify()
+        public readonly KeyPair KeyPair;
+        public readonly Msg Msg;
+        public readonly byte[] EcdsaSig;
+
+        public BenchInputs()
         {
-            keyPair = (
+            KeyPair = new(
                 Convert.FromHexString("7ef7543476bf146020cb59f9968a25ec67c3c73dbebad8a0b53a3256170dcdfe"),
                 Convert.FromHexString("03bf2e2462a3e64b941187b903156dbe9fb9b09b1e76ff5a55edf3d441dcd50822"),
                 Convert.FromHexString("04bf2e2462a3e64b941187b903156dbe9fb9b09b1e76ff5a55edf3d441dcd508227f20aebd43fb7de880b28ea03baae531c05f17d2f99940aa6a3fe1a4c788c7a1")
             );
-            msg = "Message for signing";
-            msgBytes = Encoding.UTF8.GetBytes(msg);
-        }
 
-        public enum Feature
-        {
-            SignOnly,
-            SignAndVerify,
-        }
-
-        [Benchmark(Baseline = true)]
-        [Arguments(Feature.SignAndVerify)]
-        [Arguments(Feature.SignOnly)]
-        public object Secp256k1Net(Feature feature)
-        {
+            var msg = "Message for signing";
+            var msgBytes = Encoding.UTF8.GetBytes(msg);
             var msgHash = SHA256.HashData(msgBytes);
-            using var secp256k1 = new Secp256k1();
+            Msg = new(msg, msgBytes, msgHash);
 
+            // 32-byte big endian R value, followed by a 32-byte big endian S value
+            EcdsaSig = Convert.FromHexString("8748f4a24fd0ecca9100ef947b73cbb6f11d67d151d2a900ab9fec1dce0051cc687136810ad4aba6812ad39cea0a41ba2cb04cb32d574a443f0d5c03e2dfa44f");
+        }
+    }
+
+    [CsvMeasurementsExporter]
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+    [CategoriesColumn]
+    public class EcdsaSignVerify
+    {
+        private readonly BenchInputs inputs = new BenchInputs();
+
+        [BenchmarkCategory("Sign"), Benchmark(Description = "Secp256k1Net", Baseline = true)]
+        public byte[] Secp256k1NetSign()
+        {
+            return Secp256k1NetUtil.Sign(inputs.KeyPair, inputs.Msg);
+        }
+
+        [BenchmarkCategory("Sign"), Benchmark(Description = "NBitcoin")]
+        public byte[] NBitcoinSign()
+        {
+            return NBitcoinUtil.Sign(inputs.KeyPair, inputs.Msg);
+        }
+
+        [BenchmarkCategory("Sign"), Benchmark(Description = "Nethereum")]
+        public byte[] NethereumSign()
+        {
+            return NethereumUtil.Sign(inputs.KeyPair, inputs.Msg);
+        }
+
+        [BenchmarkCategory("Sign"), Benchmark(Description = "BouncyCastle")]
+        public byte[] BouncyCastleSign()
+        {
+            return BouncyCastleUtil.Sign(inputs.KeyPair, inputs.Msg);
+        }
+
+        [BenchmarkCategory("Sign"), Benchmark(Description = "StarkBank")]
+        public byte[] StarkBankSign()
+        {
+            return StarkBankUtil.Sign(inputs.KeyPair, inputs.Msg);
+        }
+
+        [BenchmarkCategory("Sign"), Benchmark(Description = "Chainers")]
+        public byte[] ChainersSign()
+        {
+            return ChainersUtil.Sign(inputs.KeyPair, inputs.Msg);
+        }
+
+        [BenchmarkCategory("Verify"), Benchmark(Description = "Secp256k1Net", Baseline = true)]
+        public void Secp256k1NetVerify()
+        {
+            Secp256k1NetUtil.Verify(inputs.KeyPair, inputs.Msg, inputs.EcdsaSig);
+        }
+
+        [BenchmarkCategory("Verify"), Benchmark(Description = "NBitcoin")]
+        public void NBitcoinVerify()
+        {
+            NBitcoinUtil.Verify(inputs.KeyPair, inputs.Msg, inputs.EcdsaSig);
+        }
+
+        [BenchmarkCategory("Verify"), Benchmark(Description = "Nethereum")]
+        public void NethereumVerify()
+        {
+            NethereumUtil.Verify(inputs.KeyPair, inputs.Msg, inputs.EcdsaSig);
+        }
+
+        [BenchmarkCategory("Verify"), Benchmark(Description = "BouncyCastle")]
+        public void BouncyCastleVerify()
+        {
+            BouncyCastleUtil.Verify(inputs.KeyPair, inputs.Msg, inputs.EcdsaSig);
+        }
+
+        [BenchmarkCategory("Verify"), Benchmark(Description = "StarkBank")]
+        public void StarkBankVerify()
+        {
+            StarkBankUtil.Verify(inputs.KeyPair, inputs.Msg, inputs.EcdsaSig);
+        }
+    }
+
+    interface EcdsaSigner
+    {
+        static abstract byte[] Sign(KeyPair keyPair, Msg msg);
+    }
+
+    interface EcdsaVerifier
+    {
+        static abstract void Verify(KeyPair keyPair, Msg msg, byte[] signature);
+    }
+
+    class Secp256k1NetUtil : EcdsaSigner, EcdsaVerifier
+    {
+        public static byte[] Sign(KeyPair keyPair, Msg msg)
+        {
+            using var secp256k1 = new Secp256k1();
             var sig = new byte[Secp256k1.SIGNATURE_LENGTH];
-            if (!secp256k1.Sign(sig, msgHash, keyPair.PrivateKey))
+            if (!secp256k1.Sign(sig, msg.MsgHash, keyPair.PrivateKey))
                 throw new Exception();
             var serializedSig = new byte[Secp256k1.SERIALIZED_SIGNATURE_SIZE];
             if (!secp256k1.SignatureSerializeCompact(serializedSig, sig))
                 throw new Exception();
+            return serializedSig;
+        }
 
-            if (feature == Feature.SignOnly)
-                return serializedSig;
-
-            if (!secp256k1.SignatureParseCompact(sig, serializedSig))
+        public static void Verify(KeyPair keyPair, Msg msg, byte[] signature)
+        {
+            using var secp256k1 = new Secp256k1();
+            var parsedSig = new byte[Secp256k1.SIGNATURE_LENGTH];
+            if (!secp256k1.SignatureParseCompact(parsedSig, signature))
                 throw new Exception();
-
             var parsedPubKey = new byte[Secp256k1.PUBKEY_LENGTH];
             if (!secp256k1.PublicKeyParse(parsedPubKey, keyPair.PublicKeyCompressed))
                 throw new Exception();
-
-            if (!secp256k1.Verify(sig, msgHash, parsedPubKey))
+            if (!secp256k1.Verify(parsedSig, msg.MsgHash, parsedPubKey))
                 throw new Exception();
-
-            return serializedSig;
         }
+    }
 
-        [Benchmark]
-        [Arguments(Feature.SignAndVerify)]
-        [Arguments(Feature.SignOnly)]
-        public object Nbitcoin(Feature feature)
+    class NBitcoinUtil : EcdsaSigner, EcdsaVerifier
+    {
+        public static byte[] Sign(KeyPair keyPair, Msg msg)
         {
-            var msgHash = SHA256.HashData(msgBytes);
-            var ctx = new NBitcoin.Secp256k1.Context();
-            var ecPrivKey = NBitcoin.Secp256k1.ECPrivKey.Create(keyPair.PrivateKey, ctx);
-
-            var sig = ecPrivKey.SignECDSARFC6979(msgHash);
+            var ecPrivKey = NBitcoin.Secp256k1.ECPrivKey.Create(keyPair.PrivateKey);
+            var sig = ecPrivKey.SignECDSARFC6979(msg.MsgHash);
             var serializedSig = new byte[64];
             sig.WriteCompactToSpan(serializedSig);
+            return serializedSig;
+        }
 
-            if (feature == Feature.SignOnly)
-                return serializedSig;
-
-            if (!NBitcoin.Secp256k1.SecpECDSASignature.TryCreateFromCompact(serializedSig, out var parsedSig))
+        public static void Verify(KeyPair keyPair, Msg msg, byte[] signature)
+        {
+            if (!NBitcoin.Secp256k1.SecpECDSASignature.TryCreateFromCompact(signature, out var parsedSig))
                 throw new Exception("Failed to parse compact signature");
-
             var ecPubKey = NBitcoin.Secp256k1.ECPubKey.Create(keyPair.PublicKeyCompressed);
-            if (!ecPubKey.SigVerify(parsedSig, msgHash))
+            if (!ecPubKey.SigVerify(parsedSig, msg.MsgHash))
                 throw new Exception("Failed to verify signature");
-
-            return serializedSig;
         }
-        
-        [Benchmark]
-        [Arguments(Feature.SignAndVerify)]
-        [Arguments(Feature.SignOnly)]
-        public object Nethereum(Feature feature)
+    }
+
+    class NethereumUtil : EcdsaSigner, EcdsaVerifier
+    {
+        public static byte[] Sign(KeyPair keyPair, Msg msg)
         {
-            var msgHash = SHA256.HashData(msgBytes);
-            var ecPrivKey = new NethereumSigner.EthECKey(keyPair.PrivateKey, isPrivate: true);
-            var sig = ecPrivKey.Sign(msgHash);
+            var ecPrivKey = new Nethereum.Signer.EthECKey(keyPair.PrivateKey, isPrivate: true);
+            var sig = ecPrivKey.Sign(msg.MsgHash);
             var serializedSig = sig.To64ByteArray();
-
-            if (feature == Feature.SignOnly)
-                return serializedSig;
-
-            var parsedSig = NethereumSigner.EthECDSASignatureFactory.FromComponents(serializedSig);
-            var pubKey = new NethereumSigner.EthECKey(keyPair.PublicKeyCompressed, isPrivate: false);
-            if (!pubKey.Verify(msgHash, parsedSig))
-                throw new Exception("Failed to verify signature");
-
             return serializedSig;
         }
 
-        [Benchmark]
-        [Arguments(Feature.SignAndVerify)]
-        [Arguments(Feature.SignOnly)]
-        public object BouncyCastle(Feature feature)
+        public static void Verify(KeyPair keyPair, Msg msg, byte[] signature)
         {
-            // https://stackoverflow.com/a/62414581/794962
-            byte[] derSignature;
-            {
-                var curve = Org.BouncyCastle.Asn1.Sec.SecNamedCurves.GetByName("secp256k1");
-                var domain = new Org.BouncyCastle.Crypto.Parameters.ECDomainParameters(curve.Curve, curve.G, curve.N, curve.H);
-                var d = new Org.BouncyCastle.Math.BigInteger(1, keyPair.PrivateKey);
-                var keyParameters = new Org.BouncyCastle.Crypto.Parameters.ECPrivateKeyParameters(d, domain);
-                var signer = new Org.BouncyCastle.Crypto.Signers.ECDsaSigner(new Org.BouncyCastle.Crypto.Signers.HMacDsaKCalculator(new Org.BouncyCastle.Crypto.Digests.Sha256Digest()));
-                signer.Init(true, keyParameters);
-                var signature = signer.GenerateSignature(msgBytes);
-                derSignature = new Org.BouncyCastle.Asn1.DerSequence
-                (
-                    new Org.BouncyCastle.Asn1.DerInteger(new Org.BouncyCastle.Math.BigInteger(1, signature[0].ToByteArray())),
-                    new Org.BouncyCastle.Asn1.DerInteger(new Org.BouncyCastle.Math.BigInteger(1, signature[1].ToByteArray()))
-                )
-                .GetDerEncoded();
-            }
+            var parsedSig = Nethereum.Signer.EthECDSASignatureFactory.FromComponents(signature);
+            var pubKey = new Nethereum.Signer.EthECKey(keyPair.PublicKeyCompressed, isPrivate: false);
+            if (!pubKey.Verify(msg.MsgHash, parsedSig))
+                throw new Exception("Failed to verify signature");
+        }
+    }
 
-            if (feature == Feature.SignOnly)
-                return derSignature;
-
-            {
-                var curve = Org.BouncyCastle.Asn1.Sec.SecNamedCurves.GetByName("secp256k1");
-                var domain = new Org.BouncyCastle.Crypto.Parameters.ECDomainParameters(curve.Curve, curve.G, curve.N, curve.H);
-                var q = curve.Curve.DecodePoint(keyPair.PublicKeyCompressed);
-                var keyParameters = new Org.BouncyCastle.Crypto.Parameters.ECPublicKeyParameters(q, domain);
-                var verifier = new Org.BouncyCastle.Crypto.Signers.ECDsaSigner();
-                verifier.Init(false, keyParameters);
-
-                using var decoder = new Org.BouncyCastle.Asn1.Asn1InputStream(derSignature);
-                var seq = (Org.BouncyCastle.Asn1.DerSequence)decoder.ReadObject();
-                var r = (Org.BouncyCastle.Asn1.DerInteger)seq[0];
-                var s = (Org.BouncyCastle.Asn1.DerInteger)seq[1];
-                var rp = r.PositiveValue;
-                var sp = s.PositiveValue;
-
-                if (!verifier.VerifySignature(msgBytes, rp, sp))
-                {
-                    throw new Exception("Failed to verify signature");
-                }
-            }
-            return derSignature;
+    class BouncyCastleUtil : EcdsaSigner, EcdsaVerifier
+    {
+        public static byte[] Sign(KeyPair keyPair, Msg msg)
+        {
+            var curve = Org.BouncyCastle.Asn1.Sec.SecNamedCurves.GetByName("secp256k1");
+            var domain = new Org.BouncyCastle.Crypto.Parameters.ECDomainParameters(curve.Curve, curve.G, curve.N, curve.H);
+            var d = new Org.BouncyCastle.Math.BigInteger(1, keyPair.PrivateKey);
+            var keyParameters = new Org.BouncyCastle.Crypto.Parameters.ECPrivateKeyParameters(d, domain);
+            var signer = new Org.BouncyCastle.Crypto.Signers.ECDsaSigner();
+            signer.Init(true, keyParameters);
+            var signature = signer.GenerateSignature(msg.MsgHash);
+            var r = signature[0].ToByteArrayUnsigned();
+            var s = signature[1].ToByteArrayUnsigned();
+            var serializedSig = new byte[64];
+            r.CopyTo(serializedSig, 32 - r.Length);
+            s.CopyTo(serializedSig, 64 - s.Length);
+            return serializedSig;
         }
 
-        [Benchmark]
-        [Arguments(Feature.SignOnly)]
-        public object Chainers(Feature feature)
+        public static void Verify(KeyPair keyPair, Msg msg, byte[] signature)
         {
-            var msgHash = SHA256.HashData(msgBytes);
-            var sig = Cryptography.ECDSA.Secp256K1Manager.SignCompressedCompact(msgHash, keyPair.PrivateKey);
-            return sig;
+            var curve = Org.BouncyCastle.Asn1.Sec.SecNamedCurves.GetByName("secp256k1");
+            var domain = new Org.BouncyCastle.Crypto.Parameters.ECDomainParameters(curve.Curve, curve.G, curve.N, curve.H);
+            var q = curve.Curve.DecodePoint(keyPair.PublicKeyCompressed);
+            var keyParameters = new Org.BouncyCastle.Crypto.Parameters.ECPublicKeyParameters(q, domain);
+            var verifier = new Org.BouncyCastle.Crypto.Signers.ECDsaSigner();
+            verifier.Init(false, keyParameters);
+            var rp = new Org.BouncyCastle.Math.BigInteger(1, signature.Take(32).ToArray());
+            var sp = new Org.BouncyCastle.Math.BigInteger(1, signature.Skip(32).ToArray());
+            if (!verifier.VerifySignature(msg.MsgHash, rp, sp))
+                throw new Exception("Failed to verify signature");
         }
+    }
 
-        [Benchmark]
-        [Arguments(Feature.SignAndVerify)]
-        [Arguments(Feature.SignOnly)]
-        public object StarkBank(Feature feature)
+    class StarkBankUtil : EcdsaSigner, EcdsaVerifier
+    {
+        public static byte[] Sign(KeyPair keyPair, Msg msg)
         {
             var privateKey = EllipticCurve.PrivateKey.fromString(keyPair.PrivateKey);
-            var sig = EllipticCurve.Ecdsa.sign(msg, privateKey);
+            var sig = EllipticCurve.Ecdsa.sign(msg.MsgString, privateKey);
+            var r = sig.r.ToByteArray(isUnsigned: true, isBigEndian: true);
+            var s = sig.s.ToByteArray(isUnsigned: true, isBigEndian: true);
+            var serializedSig = new byte[64];
+            r.CopyTo(serializedSig, 32 - r.Length);
+            s.CopyTo(serializedSig, 64 - s.Length);
+            return serializedSig;
+        }
 
-            if (feature == Feature.SignOnly)
-                return sig;
-
-            var parsedSig = new EllipticCurve.Signature(r: sig.r, s: sig.s);
+        public static void Verify(KeyPair keyPair, Msg msg, byte[] signature)
+        {
+            var r = new BigInteger(signature.Take(32).ToArray(), isUnsigned: true, isBigEndian: true);
+            var s = new BigInteger(signature.Skip(32).ToArray(), isUnsigned: true, isBigEndian: true);
+            var parsedSig = new EllipticCurve.Signature(r, s);
             var pubKey = EllipticCurve.PublicKey.fromString(keyPair.PublicKeyUncompressed.Skip(1).ToArray());
-            if (!EllipticCurve.Ecdsa.verify(msg, parsedSig, pubKey))
+            if (!EllipticCurve.Ecdsa.verify(msg.MsgString, parsedSig, pubKey))
                 throw new Exception("Failed to verify signature");
+        }
+    }
 
+    class ChainersUtil : EcdsaSigner
+    {
+        public static byte[] Sign(KeyPair keyPair, Msg msg)
+        {
+            var sig = Cryptography.ECDSA.Secp256K1Manager.SignCompressedCompact(msg.MsgHash, keyPair.PrivateKey);
             return sig;
         }
     }
@@ -193,7 +261,7 @@ namespace Secp256k1Net.Bench
     {
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<EcdsaSignVerify>();
+            BenchmarkRunner.Run<EcdsaSignVerify>();
             Console.WriteLine("Benchmarks done");
         }
     }
